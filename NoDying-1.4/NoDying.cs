29a8c7fd6084060ab18e77a56061151acd2035a0
wolfangomg <32171402+wolfangomg@users.noreply.github.com>
2023-03-28 15:27:19 +0200

    update
    asdas

---
+++ NoDying-1.4/NoDying.cs
@@ -0,0 +1,12 @@
+﻿using Modding;
+
+namespace NoDying;
+
+public class NoDying : Mod, ITogglableMod
+{
+    public override string GetVersion() => "1.0.0.0";
+    
+    public override void Initialize() => ModHooks.Instance.TakeHealthHook += DontAllowDying; 
+    public void Unload() => ModHooks.Instance.TakeHealthHook -= DontAllowDying;
+    private static int DontAllowDying(int damage) => PlayerData.instance.health - damage <= 0 ? 0 : damage;
+}
---
+++ NoDying-1.5/NoDying.cs
@@ -0,0 +1,12 @@
+﻿using Modding;
+
+namespace NoDying;
+
+public class NoDying : Mod, ITogglableMod
+{
+    public override string GetVersion() => "1.0.0.0";
+    
+    public override void Initialize() => ModHooks.TakeHealthHook += DontAllowDying; 
+    public void Unload() => ModHooks.TakeHealthHook -= DontAllowDying;
+    private static int DontAllowDying(int damage) => PlayerData.instance.health - damage <= 0 ? 0 : damage;
+}
